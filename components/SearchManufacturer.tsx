f8690d43a0877e3239bc3ba0a92dc6f6214b1544
Anirudh Salaria <salariaanirudh@gmail.com>
2023-06-27 14:48:13 +0530

    updates

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,10 +1,13 @@
+import { Combobox, Transition } from '@headlessui/react';
 import { SearchManufacturerProps } from '@/types'
 import React from 'react'
 
 const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufacturerProps) => {
   return (
-    <div>
-      SearchManufacturer
+    <div className='search-manufacturer'>
+      <Combobox>
+        
+      </Combobox>
     </div>
   )
 }
