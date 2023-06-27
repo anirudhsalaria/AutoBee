758319ee218bc354b0b707023bb309690349e42a
Anirudh Salaria <salariaanirudh@gmail.com>
2023-06-27 20:57:08 +0530

    Update SearchBar.tsx

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,21 +1,21 @@
 "use client";
 import { useState } from "react";
 
-import {SearchManufacturer} from "./";
+import { SearchManufacturer } from "./";
 
 const SearchBar = () => {
-    const [manufacturer, setManufacturer] = useState('');
+  const [manufacturer, setManufacturer] = useState('');
 
-    const handleSearch = () => {}
+  const handleSearch = () => { }
 
   return (
     <form className='searchbar' onSubmit={handleSearch}>
-        <div className="searchbar__item">
-            <SearchManufacturer
-                manufacturer= {manufacturer}
-                setManufacturer={setManufacturer}
-            />
-        </div>
+      <div className="searchbar__item">
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManufacturer={setManufacturer}
+        />
+      </div>
     </form>
   )
 }
