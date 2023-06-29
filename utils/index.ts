60c599f48d6605d315f5113dfe942754da8c8282
Anirudh Salaria <salariaanirudh@gmail.com>
2023-06-29 14:32:32 +0530

    Create index.ts

---
+++ utils/index.ts
@@ -0,0 +1,13 @@
+export async function fetchCars() {
+    const headers = {
+        'X-RapidAPI-Key': '7564cc5fa2mshe1ccf4580d12d5dp1ee617jsn1dadedd6cd74',
+		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+    }
+
+    const response = await fetch ('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars',{
+        headers: headers,
+    });
+
+    const result = await response.json();
+    return result;
+}
