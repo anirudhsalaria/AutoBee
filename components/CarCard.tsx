e2c919ca7bc61f098865078efaad4971d771a3d0
Anirudh Salaria <salariaanirudh@gmail.com>
2023-06-30 14:47:09 +0530

    Update CarCard.tsx

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -35,6 +35,36 @@ const CarCard = ({car}: CarCardProps) => {
       <div className='relative w-full h-40 my-3 object-contain'>
         <Image src="/hero.png" alt='car model' fill priority className='object-contain'/>
       </div>
+
+      <div className='relative flex w-full mt-2'>
+        <div className='flex group-hover:invisible w-full justify-between text-gray'>
+            <div className='flex flex-col justify-center items-center gap-2'>
+                <Image src="/steering-wheel.svg" width={20} height={20} alt='steering'/>
+                <p className='text-[14px]'>
+                    {transmission === 'a' ? 'Automatic' : 'Manual'}
+                </p>
+            </div>
+            <div className='flex flex-col justify-center items-center gap-2'>
+                <Image src="/tire.svg" width={20} height={20} alt='tire'/>
+                <p className='text-[14px]'>
+                    {drive.toUpperCase()}
+                </p>
+            </div>
+            <div className='flex flex-col justify-center items-center gap-2'>
+                <Image src="/gas.svg" width={20} height={20} alt='gas'/>
+                <p className='text-[14px]'>
+                    {city_mpg} KM/L
+                </p>
+            </div>
+        </div>
+        
+        <div className='car-card__btn-container'>
+            <CustomButton
+                title='View More'
+                containerStyles='w-full py-[16px] rounded-full bg-primary-blue'
+            />
+        </div>
+      </div>
     </div>
   )
 }
