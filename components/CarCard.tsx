695e73446d7f4a7d3ca5c0c9b3e167fb64492dc3
Anirudh Salaria <salariaanirudh@gmail.com>
2023-07-04 21:18:02 +0530

    changes

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,4 +1,4 @@
-"use Client";
+"use client";
 import { useState } from 'react';
 import Image from 'next/image';
 import { CarProps } from '@/types';
@@ -11,6 +11,8 @@ interface CarCardProps {
 
 const CarCard = ({ car }: CarCardProps) => {
   const { city_mpg, year, make, model, transmission, drive } = car;
+
+  const [isOpen, setIsOpen] = useState(false);
 
   const carRent = calculateCarRent(city_mpg, year);
 
@@ -37,7 +39,7 @@ const CarCard = ({ car }: CarCardProps) => {
       </div>
 
       <div className='relative flex w-full mt-2'>
-        <div className='flex group-hover:invisible w-full justify-between text-gray'>
+        <div className='flex group-hover:invisible w-full justify-between text-grey'>
           <div className='flex flex-col justify-center items-center gap-2'>
             <Image src="/steering-wheel.svg" width={20} height={20} alt='steering' />
             <p className='text-[14px]'>
@@ -58,10 +60,13 @@ const CarCard = ({ car }: CarCardProps) => {
           </div>
         </div>
 
-        <div className='car-card__btn-container'>
+        <div className="car-card__btn-container">
           <CustomButton
             title='View More'
-            containerStyles='w-full py-[16px] rounded-full bg-primary-blue'
+            containerStyles='w-[23rem] py-[16px] rounded-full bg-primary-blue'
+            textStyles="text-white text-[14px] leading-[17px] font-bold"
+            rightIcon="/right-arrow.svg"
+            handleClick={() => setIsOpen(true)}
           />
         </div>
       </div>
